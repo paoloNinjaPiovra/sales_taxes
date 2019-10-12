fcf77a20b11273a05acd93b742d7d63e16cc351b
paolofasoli <p.fasoli@gmail.com>
2019-10-12 09:09:02 +0200

    salesTaxes

--- src/main/java/com/sales/taxes/App.java
+++ src/main/java/com/sales/taxes/App.java
@@ -18,13 +18,18 @@ public class App
             System.out.println();
 
             RecipeOutput recipeOutput2 = calculate.calculate("input2");
-            System.out.println("Output 1:");
+            System.out.println("Output 2:");
             System.out.println(recipeOutput2.toString());
             System.out.println();
 
             RecipeOutput recipeOutput3 = calculate.calculate("input3");
-            System.out.println("Output 1:");
+            System.out.println("Output 3:");
             System.out.println(recipeOutput3.toString());
+            System.out.println();
+
+            RecipeOutput recipeOutput4 = calculate.calculate("input4");
+            System.out.println("Output 4:");
+            System.out.println(recipeOutput4.toString());
             System.out.println();
         } catch (Exception e) {
             System.out.println(e);
